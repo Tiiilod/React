f43729ac10121c3c4ba89c68e647cb5913e2d77b
Tiiilod <turky2202@student.abudzar.sch.id>
2025-01-13 13:56:07 +0700

    update

--- coba/src/main.jsx
+++ coba/src/main.jsx
@@ -1,11 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-
-
-
-
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 //meeting-4
 import Form from "./meeting/meeting-4/Praktek/fragment/form.jsx";
@@ -16,20 +10,18 @@ import Pages from "./meeting/meeting-4/Praktek/page/page.jsx";
 //meeting-5
 
 //meeting-8
-import Array2 from './meeting-8/coba/coba-array-8.jsx';
-import Numberss from './meeting-8/coba/coba-number-8.jsx';
-import Counter from './meeting-8/praktek/soal-1.jsx';
-import LoginStatus from './meeting-8/praktek/soal-2.jsx';
-import SimpleCalculator from './meeting-8/praktek/soal-3.jsx';
+import Array2 from "./meeting-8/coba/coba-array-8.jsx";
+import Numberss from "./meeting-8/coba/coba-number-8.jsx";
+import Counter from "./meeting-8/praktek/soal-1.jsx";
+import LoginStatus from "./meeting-8/praktek/soal-2.jsx";
+import SimpleCalculator from "./meeting-8/praktek/soal-3.jsx";
 //meeting-9
-import SimpleNameList from './meeting/meeting-9/coba/coba-array-9.jsx';
-import UpdateStateWithMap from './meeting/meeting-9/coba/coba-map-9.jsx';
-import Produk from './meeting/meeting-9/praktek/soal1.jsx';
+import SimpleNameList from "./meeting/meeting-9/coba/coba-array-9.jsx";
+import UpdateStateWithMap from "./meeting/meeting-9/coba/coba-map-9.jsx";
+import Produk from "./meeting/meeting-9/praktek/soal1.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Register  />
-  </StrictMode>,
-)
-
+    <Register />
+  </StrictMode>
+);
