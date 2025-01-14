c5ff8a370e2fe6c7484d596a4f91c78ab6d843ae
Tiiilod <turky2202@student.abudzar.sch.id>
2025-01-14 13:14:52 +0700

    update

--- react-tailwind/src/main.jsx
+++ react-tailwind/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+//meeting-9
 import Produk from "./meeting/meeting-9/Praktek/soal1.jsx";
+//meeting-10
+import Soal1 from "./meeting/meeting-10/Praktek/soal1.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Produk />
+    <Soal1 />
   </StrictMode>,
 )
