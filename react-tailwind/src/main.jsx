99f8b24e87b2c6fa9a5f15f6b7b8616dedd406d1
Tiiilod <turky2202@student.abudzar.sch.id>
2025-02-17 10:16:31 +0700

    update meeting-17

--- react-tailwind/src/main.jsx
+++ react-tailwind/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import LoginMas from "./meeting/meeting-17/praktek-1/jwt-login";
+
+
 import Axioshop17 from "./meeting/meeting-17/praktek-1/jwt-path";
 import Menu from "./meeting/meeting-17/praktek-2/menu";
+import CreateUser from "./meeting/meeting-17/coba/coba";
 
 
 
@@ -11,6 +13,6 @@ import Menu from "./meeting/meeting-17/praktek-2/menu";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <RouterProvider router={router} /> */}
-    <Menu  />
+    <CreateUser  />
   </StrictMode>
 );
