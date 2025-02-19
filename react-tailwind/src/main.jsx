c8b6bd1004146f29bc8f9587d4a4b45ab61fe868
Tiiilod <turky2202@student.abudzar.sch.id>
2025-02-19 13:52:41 +0700

    update

--- react-tailwind/src/main.jsx
+++ react-tailwind/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 
 import Axioshop18 from "./meeting/meeting-18/Praktek-1/jwt-path";
 import Menu18 from "./meeting/meeting-18/Praktek-2/menu";
+import AxiosStore2 from "./meeting/meeting-18/Praktek-1/jwt-map";
 
 
 
